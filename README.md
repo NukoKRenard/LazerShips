1ca61620930adaec071de65a168a1bf0bc911091
NukoKRenard <skeanobo@gmail.com>
2025-01-09 10:11:12 -0500

    Updated the run.bat and README.md files.

--- README.md
+++ README.md
@@ -2,17 +2,17 @@
 This is a game I made from scratch using the OpenGL graphics engine. I did not use a game engine to make this. In this game you play as a blue starship, your goal is to destroy all of the enemy (red) starships.
 
 ## Running the game (Windows)
-The provided run.bat file will set the common directoy and run the game.
+To run the game double click the ```run.bat``` file in the foldr
 
-**Please ensure a python 3 environment is installed on the computer**
+Please ensure a python 3 interpreter is present on the computer and is a part of path.
 
-**Please ensure that the following python packages are installed before running:**
+**The following packages are required by the game to run. If not installed the ```run.bat``` file will attempt to install them automatically.**
 * numpy
 * pyglm
 * pyopengl
 * pygame
 
-To install python packages, use the terminal with the command:
+To install python packages manually, use the terminal with the command:
 ```pip install <package-name>```
 or
 ```python -m pip install <package-name>```
--- run.bat
+++ run.bat
@@ -4,27 +4,47 @@ pushd %~dp0
 setlocal ENABLEDELAYEDEXPANSION
 
 :retry
-python main.py
+python main.py > log.txt
 
 if %ERRORLEVEL% NEQ 0 (
+        echo "\n" >> log.txt
+        echo "ERROR DEBUG:" >> log.txt
         echo An error occured when running the game.
-        python --version > nul
+        python --version >> log.txt
         if !ERRORLEVEL! NEQ 0 (
-            echo python is not installed. You can install python 3 from python.org or the microsoft store. Python is required for this game to work.
-            echo We can redirect you to python.org. Press any key to visit the site...
+            echo No python interpreter is installed on this computer. Please install the newest python version from python.org. Python is required for this game to work.
+	    echo Please download the windows 64 bit installer. Please also make sure you enable the setting to add it to PATH.
+	    echo To open python.org press any key...
             pause
-            start https://python.org
+            start https://www.python.org/downloads/windows/
             exit
         )
+
         for %%p in ("pyglm","pyopengl","pygame","numpy") do (
-            python -c "import %%~p" > nul
+
+            if %%p EQU "pyglm" python -c "import glm" >> log.txt
+            if %%p EQU "pyopengl" python -c "import OpenGL.GL" >> log.txt
+            if %%p NEQ "pyglm" if %%p NEQ "pyopengl" python -c "import %%~p" >> log.txt
+
             if !ERRORLEVEL! NEQ 0 (
-                echo Python package %%p is not installed. To allow the game to work this package must be installed.
-                echo To proceed with automatic installation, press Enter, you can also install manually using "pip install %%~p"
+                echo Python package %%~p is not installed. To allow the game to work this package must be installed.
+                echo To proceed with automatic installation, press Enter, you can also install manually using "pip install %%~p" or "python -m pip install %%~p"
                 pause
-                echo installing %%p...
-                pip install %%p
-                echo %%p installed. Retrying...
+                echo installing %%~p...
+                python -m pip install %%~p >> log.txt
+                echo %%p Testing installation...
+
+                if %%p EQU "pyglm" python -c "import glm" >> log.txt
+                if %%p EQU "pyopengl" python -c "import OpenGL.GL" >> log.txt
+                if %%p NEQ "pyglm" if %%p NEQ "pyopengl" python -c "import %%~p" >> log.txt
+
+                if !ERRORLEVEL! NEQ 0 (
+                    echo Installation FAILED. Please install %%~p manually using "pip install %%~p" or "python -m pip install %%~p"
+                    pause
+                    exit
+                )
+
+                echo test passed.
             )
         )
         goto retry
